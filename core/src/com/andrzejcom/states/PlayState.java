05c65d510347c0d593df99aaa0b5326fcc048d30
Andrzej1991 <andrzej.perski@hotmail.com>
2017-06-13 09:43:45 +0200

    configure jump for bird and adds other feathures

--- core/src/com/andrzejcom/states/PlayState.java
+++ core/src/com/andrzejcom/states/PlayState.java
@@ -2,6 +2,8 @@ package com.andrzejcom.states;
 
 import com.andrzejcom.FlappyDemo;
 import com.andrzejcom.sprites.Bird;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
 /**
@@ -10,16 +12,20 @@ import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
 public class PlayState extends State {
     private Bird bird;
+    private Texture bg;
 
     protected PlayState(GameStateManager gsm) {
         super(gsm);
         bird = new Bird(50, 100);
         cam.setToOrtho(false, FlappyDemo.WIDTH / 2, FlappyDemo.HEIGHT / 2);
+        bg = new Texture("bg.png");
     }
 
     @Override
     protected void handlerInput() {
-
+        if (Gdx.input.justTouched()) {
+            bird.jump();
+        }
     }
 
     @Override
@@ -32,6 +38,7 @@ public class PlayState extends State {
     public void render(SpriteBatch sb) {
         sb.setProjectionMatrix(cam.combined);
         sb.begin();
+        sb.draw(bg, cam.position.x - (cam.viewportWidth / 2), 0);
         sb.draw(bird.getBird(), bird.getPosition().x, bird.getPosition().y);
         sb.end();
 

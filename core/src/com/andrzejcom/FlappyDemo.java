f04fc93b8c212caf823f91c3c71cc47520df9c77
Andrzej1991 <andrzej.perski@hotmail.com>
2017-06-13 11:45:24 +0200

    added music and sound for flappy

--- core/src/com/andrzejcom/FlappyDemo.java
+++ core/src/com/andrzejcom/FlappyDemo.java
@@ -4,6 +4,7 @@ import com.andrzejcom.states.GameStateManager;
 import com.andrzejcom.states.MenuState;
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.audio.Music;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
@@ -12,6 +13,8 @@ public class FlappyDemo extends ApplicationAdapter {
     public static final int HEIGHT = 800;
     public static final String TITLE = "Flappy bird";
 
+    private Music music;
+
     private GameStateManager gsm;
     private SpriteBatch batch;
 
@@ -19,6 +22,10 @@ public class FlappyDemo extends ApplicationAdapter {
     public void create() {
         batch = new SpriteBatch();
         gsm = new GameStateManager();
+        music = Gdx.audio.newMusic(Gdx.files.internal("music.mp3"));
+        music.setLooping(true);
+        music.setVolume(0.1f);
+        music.play();
         Gdx.gl.glClearColor(1, 0, 0, 1);
         gsm.push(new MenuState(gsm));
     }
@@ -29,5 +36,11 @@ public class FlappyDemo extends ApplicationAdapter {
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
         gsm.update(Gdx.graphics.getDeltaTime());
         gsm.render(batch);
+    }
+
+    @Override
+    public void dispose() {
+        super.dispose();
+        music.dispose();
     }
 }

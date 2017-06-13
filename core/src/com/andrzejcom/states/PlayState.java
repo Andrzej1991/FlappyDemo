98976d4ed42a517316073a14289289889c32f54b
Andrzej1991 <andrzej.perski@hotmail.com>
2017-06-13 09:55:19 +0200

    addingd tube and logic for showing it on play state

--- core/src/com/andrzejcom/states/PlayState.java
+++ core/src/com/andrzejcom/states/PlayState.java
@@ -2,6 +2,7 @@ package com.andrzejcom.states;
 
 import com.andrzejcom.FlappyDemo;
 import com.andrzejcom.sprites.Bird;
+import com.andrzejcom.sprites.Tube;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
@@ -13,12 +14,14 @@ import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 public class PlayState extends State {
     private Bird bird;
     private Texture bg;
+    private Tube tube;
 
     protected PlayState(GameStateManager gsm) {
         super(gsm);
         bird = new Bird(50, 100);
         cam.setToOrtho(false, FlappyDemo.WIDTH / 2, FlappyDemo.HEIGHT / 2);
         bg = new Texture("bg.png");
+        tube = new Tube(100);
     }
 
     @Override
@@ -40,6 +43,8 @@ public class PlayState extends State {
         sb.begin();
         sb.draw(bg, cam.position.x - (cam.viewportWidth / 2), 0);
         sb.draw(bird.getBird(), bird.getPosition().x, bird.getPosition().y);
+        sb.draw(tube.getTopTube(), tube.getPosTopTube().x, tube.getPosTopTube().y);
+        sb.draw(tube.getBottomTube(), tube.getPosBotTube().x, tube.getPosBotTube().y);
         sb.end();
 
     }
